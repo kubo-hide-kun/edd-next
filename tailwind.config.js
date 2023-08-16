2418c7e657e933dfd737d3a40c5a17965aca96a1
kubo-hide-kun <hide44041@gmail.com>
2023-08-17 01:20:13 +0900

    chore: initial commit :sparkles:

---
+++ tailwind.config.js
@@ -0,0 +1,71 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const range = require('lodash/range');
+
+const screenSizes = {
+  'smartphone-lg': '428px',
+  tablet: '640px',
+  laptop: '1024px',
+  desktop: '1280px',
+};
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // eslint-disable-next-line no-undef
+  content: [path.join(__dirname, 'src/**/!(*.d).{ts,js,jsx,tsx}')],
+  theme: {
+    spacing: {
+      px: '1px',
+      ...range(0, 900).reduce((acc, i) => {
+        acc[i] = `${i}px`;
+        return acc;
+      }, {}),
+    },
+    maxWidth: {
+      px: '1px',
+      ...range(1, 900).reduce((acc, i) => {
+        acc[i] = `${i}px`;
+        return acc;
+      }, {}),
+    },
+    minWidth: {
+      px: '1px',
+      ...range(1, 800).reduce((acc, i) => {
+        acc[i] = `${i}px`;
+        return acc;
+      }, {}),
+    },
+    fontSize: {
+      xxs: '10px',
+      xs: '12px',
+      'sm-mini': '13px',
+      sm: '14px',
+      base: '16px',
+      lg: '18px',
+      xl: '20px',
+      '2xl': '24px',
+      '3xl': '30px',
+      '4xl': '36px',
+      '5xl': '48px',
+      '6xl': '64px',
+      '7xl': '72px',
+      '8xl': '80px',
+      '9xl': '96px',
+    },
+    screens: screenSizes,
+    extend: {
+      colors: {
+        line: {
+          green: {
+            DEFAULT: 'var(--line-green)',
+            dark: 'var(--line-green-dark)',
+          },
+        },
+      },
+      animation: {
+        'spin-slow': 'spin 2s linear infinite',
+      },
+    },
+  },
+  plugins: ['tailwindcss', 'postcss-preset-env'],
+};
