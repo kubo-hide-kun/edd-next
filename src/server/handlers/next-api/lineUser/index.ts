5a9a87d97e31994e6064a2f3be6736d10daf88c4
kubo-hide-kun <hide44041@gmail.com>
2023-09-03 22:31:32 +0900

    feat: wip :bug:

--- src/server/handlers/next-api/lineUser/index.ts
+++ src/server/handlers/next-api/lineUser/index.ts
@@ -3,17 +3,17 @@ import { Api } from '~/server/handlers/next-api';
 export const PATH = '' as const;
 const INDIVIDUAL_PATH = '/api/user/line/[lineId]' as const;
 
-export class UserApi extends Api<typeof PATH, typeof INDIVIDUAL_PATH> {
+export class LineUserApi extends Api<typeof PATH, typeof INDIVIDUAL_PATH> {
   constructor() {
     super('UserApi', PATH, INDIVIDUAL_PATH);
   }
 }
 
-const userApi = new UserApi();
+const lineUserApi = new LineUserApi();
 
-console.log('userApi', userApi.individualConnectHandlers);
+console.log('userApi', lineUserApi.individualConnectHandlers);
 
-userApi.individualConnectHandlers.get = async (request, response) => {
+lineUserApi.individualConnectHandlers.get = async (request, response) => {
   const { lineId } = request.query;
   if (!lineId) {
     response.status(400).json({
@@ -23,12 +23,12 @@ userApi.individualConnectHandlers.get = async (request, response) => {
     return;
   }
 
-  userApi.init({
+  lineUserApi.init({
     ipAddress: request.ipAddress,
     authorization: request.authorization,
   });
 
-  const { findUserByLineIdUsecase } = userApi.application.usecases;
+  const { findUserByLineIdUsecase } = lineUserApi.application.usecases;
   const found = await findUserByLineIdUsecase.invoke(lineId);
 
   response.status(200).json({
@@ -36,7 +36,7 @@ userApi.individualConnectHandlers.get = async (request, response) => {
   });
 };
 
-const connect = userApi.createConnect();
-const individualConnect = userApi.createIndividualConnect();
+const connect = lineUserApi.createConnect();
+const individualConnect = lineUserApi.createIndividualConnect();
 
 export { connect, individualConnect };
