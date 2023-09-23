4f45da907e748854afdc861a089102c9892dc3f7
Kurichi <yuuyapp080@gmail.com>
2023-09-23 18:45:02 +0900

    refactor: コメントアウト部分を削除

--- src/server/infrastructures/prisma/index.ts
+++ src/server/infrastructures/prisma/index.ts
@@ -21,49 +21,4 @@ export class PrimaInfrastructure {
   get client() {
     return this._client;
   }
-
-  // /**
-  //  * 一覧取得（一覧と件数を同時に取得する）
-  //  */
-  // async customFindMany<
-  //   ModelName extends ValueOf<{
-  //     [K in keyof PrismaClient]: PrismaClient[K] extends {
-  //       findMany: unknown;
-  //       count: unknown;
-  //     }
-  //       ? K
-  //       : never;
-  //   }>
-  // >(
-  //   modelName: ModelName,
-  //   {
-  //     size = 99,
-  //     page = 1,
-  //     ...args
-  //   }: Omit<
-  //     Parameters<PrismaClient[ModelName]['findMany']>[0],
-  //     'skip' | 'take'
-  //   > & {
-  //     page?: number;
-  //     size?: number;
-  //   }
-  // ) {
-  //   const query = {
-  //     ...args,
-  //     take: size,
-  //     skip: (page - 1) * size,
-  //   };
-
-  //   const [founds, count] = await this.client.$transaction([
-  //     this.client[modelName].findMany(query),
-  //     this.client[modelName].count({ where: args.where }),
-  //   ]);
-
-  //   return {
-  //     items: founds,
-  //     size: size,
-  //     page: page,
-  //     total: count,
-  //   };
-  // }
 }
