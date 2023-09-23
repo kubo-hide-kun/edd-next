f3a65b274d0313550d2034979db0e06ab1c6bfb6
kubo-hide-kun <hide44041@gmail.com>
2023-09-24 02:24:06 +0900

    feat: create line webhook endpoint :sparkles:

--- src/server/handlers/webhook-line/index.ts
+++ src/server/handlers/webhook-line/index.ts
@@ -1,49 +1,87 @@
 import { WebhookRequestBody, middleware } from '@line/bot-sdk';
 import { Middleware } from '@line/bot-sdk/lib/middleware';
-import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
+import { NextApiHandler } from 'next';
+import { Application } from '~/server/application';
 
-const runMiddleware = (
-  request: NextApiRequest,
-  response: NextApiResponse,
-  middleware: Middleware
-) => {
-  return new Promise<void>((resolve, reject) => {
-    middleware(request, response, (error) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve();
+class lineWebhookApi {
+  private _middleware: Middleware;
+
+  constructor() {
+    this._middleware = middleware({
+      channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN!,
+      channelSecret: process.env.LINE_CHANNEL_SECRET!,
+    });
+  }
+
+  /**
+   * @description LINEのWebhook APIのエントリーポイントです。
+   */
+  public handler: NextApiHandler = async (request, response) => {
+    if (request.method !== 'POST') {
+      response.status(405).end();
+      return;
+    }
+
+    const body = request.body;
+    if (!this._isWebhookRequestBody(body)) {
+      response.status(400).end();
+      return;
+    }
+
+    await this._runMiddleware(request, response);
+
+    const application = new Application({
+      ipAddress: 'anonymous',
+    });
+
+    const tasks = this._createTasksByEvents(body.events, application);
+
+    await Promise.all(tasks);
+  };
+
+  /**
+   * @description LINEのWebhook APIのリクエストボディが正しいかどうかを判定するためのメソッドです。
+   */
+  private _isWebhookRequestBody(body: unknown): body is WebhookRequestBody {
+    if (typeof body !== 'object' || body === null) return false;
+    if (!Array.isArray(body['events'])) {
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * @description LINEのWebhook APIのミドルウェアを実行するためのメソッドです。
+   */
+  private _runMiddleware: NextApiHandler = (request, response) => {
+    return new Promise<void>((resolve, reject) => {
+      this._middleware(request, response, (error) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      });
+    });
+  };
+
+  /**
+   * @description LINEのWebhook APIのイベントから、タスクを生成するためのメソッドです。
+   */
+  private _createTasksByEvents(
+    events: WebhookRequestBody['events'],
+    application: Application
+  ) {
+    const { handleLineMessageEvent } = application.usecases;
+    return events.map((event) => {
+      switch (event.type) {
+        case 'message':
+          return handleLineMessageEvent.invoke({
+            event,
+          });
       }
     });
-  });
-};
-
-const _isWebhookRequestBody = (body: unknown): body is WebhookRequestBody => {
-  if (typeof body !== 'object' || body === null) return false;
-  if (!Array.isArray(body['events'])) {
-    return false;
   }
-  return true;
-};
+}
 
-export const lineWebhookHandler: NextApiHandler = async (request, response) => {
-  if (request.method !== 'POST') {
-    response.status(405).end();
-    return;
-  }
-
-  const body = request.body;
-  if (!_isWebhookRequestBody(body)) {
-    response.status(400).end();
-    return;
-  }
-
-  const _middleware = middleware({
-    channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN!,
-    channelSecret: process.env.LINE_CHANNEL_SECRET!,
-  });
-
-  await runMiddleware(request, response, _middleware);
-
-  await Promise.all([]);
-};
+export const handler = new lineWebhookApi().handler;
