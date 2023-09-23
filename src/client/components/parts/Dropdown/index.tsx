6307fe0210563555769d2b96fceae0ee994a9911
kubo-hide-kun <hide44041@gmail.com>
2023-09-24 00:32:39 +0900

    refactor: update nits of dropdown :recycle:

--- src/client/components/parts/Dropdown/index.tsx
+++ src/client/components/parts/Dropdown/index.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import { FC, useState } from 'react';
+import { FC, MouseEventHandler, useCallback, useMemo, useState } from 'react';
 import { SortVector } from '~/client/components/vectors/Sort';
 
 export const Dropdown: FC<{
@@ -13,17 +13,31 @@ export const Dropdown: FC<{
   const [selectedId, setSelectedId] = useState(items[0].id);
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleItemClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    const { id } = event.currentTarget.dataset;
-    if (!id) {
-      return;
-    }
-    setSelectedId(id);
-    onChange?.(id);
-    setIsOpen(false);
-  };
+  const handleButtonClick: MouseEventHandler<HTMLButtonElement> = useCallback(
+    (event) => {
+      event.stopPropagation();
+      setIsOpen((prev) => !prev);
+    },
+    [setIsOpen]
+  );
 
-  const selectedItem = items.find((item) => item.id === selectedId);
+  const handleItemClick: MouseEventHandler<HTMLButtonElement> = useCallback(
+    (event) => {
+      const { id } = event.currentTarget.dataset;
+      if (!id) {
+        return;
+      }
+      setSelectedId(id);
+      onChange?.(id);
+      setIsOpen(false);
+    },
+    [setSelectedId, onChange, setIsOpen]
+  );
+
+  const selectedItem = useMemo(
+    () => items.find((item) => item.id === selectedId),
+    [items, selectedId]
+  );
 
   return (
     <div className={classNames('relative inline-block text-left', className)}>
@@ -33,7 +47,7 @@ export const Dropdown: FC<{
           id="options-menu"
           aria-haspopup="true"
           aria-expanded="true"
-          onClick={() => setIsOpen(!isOpen)}
+          onClick={handleButtonClick}
         >
           <span>{selectedItem?.label}</span>
           <SortVector />
@@ -41,16 +55,24 @@ export const Dropdown: FC<{
         <div
           className={classNames(
             'absolute left-0 top-0 z-10 w-88 overflow-hidden rounded-md bg-white drop-shadow-lg transition-all duration-300',
-            [isOpen ? 'visible' : 'invisible']
+            {
+              visible: isOpen,
+              invisible: !isOpen,
+            }
           )}
           role="none"
         >
           {items.map((item) => (
             <button
               key={item.id}
-              className="block max-h-24 w-full text-center text-sm text-surface-base-2 transition-all duration-300"
+              className={classNames(
+                'block max-h-24 w-full text-center text-sm text-surface-base-2 transition-all duration-300',
+                {
+                  'h-36': isOpen,
+                  'h-0': !isOpen,
+                }
+              )}
               data-id={item.id}
-              style={{ height: isOpen ? '36px' : '0' }}
               role="menuitem"
               onClick={handleItemClick}
             >
